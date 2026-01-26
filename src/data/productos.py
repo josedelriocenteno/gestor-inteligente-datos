4cd5bc446f368e0d071a8f01b783e017eaf4d639
josemigueldelrio <josemiguel.delrio.centeno@gregoriofer.com>
2026-01-26 12:58:16 +0100

    producto

--- src/data/productos.py
+++ src/data/productos.py
@@ -1,8 +1,25 @@
 from dataclasses import dataclass, field
 
 @dataclass
-class productos():
-    nombre:str
-    precio:float
-    dict_productos = dict(default_factory=dict)
+class Producto:
+    nombre: str
+    precio: float
+    dict_productos: dict[str, float] = field(default_factory=dict, init=False)
 
+    def añadir_producto(self, nombre_producto: str, valor_producto: float):
+        self.dict_productos[nombre_producto] = valor_producto
+        
+    def modificar_precio_producto(self, nombre_producto: str, valor_producto: float):
+        try:
+            self.dict_productos[nombre_producto] = valor_producto
+        except:
+            print("Producto no existe")
+            
+    def eliminar_producto(self, nombre_producto: str):
+        try:
+            del self.dict_productos[nombre_producto]
+        except:
+            print("Producto no existe")
+            
+    def mostrar_productos(self):
+        print(self.dict_productos)
